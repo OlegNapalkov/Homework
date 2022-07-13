03b98efde6cd66ca04e1efa34d55fe7c54538add
Олег <nap1994@list.ru>
2022-07-13 21:33:59 +0300

    Задача с четными и нечетными числами

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+Console.WriteLine("Это число четное");
+}
+else
+{
+Console.WriteLine("Это число нечетное");
+}
