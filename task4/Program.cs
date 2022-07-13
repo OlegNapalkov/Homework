4cd9e2f364613d10b711eca6b954971b419c0197
Олег <nap1994@list.ru>
2022-07-13 21:21:39 +0300

    Задача с нахождением максимального из 3х чисел

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число_1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число_2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число_3: ");
+int N3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(N1>N2)
+max = N1;
+else
+max = N2;
+if(max > N3)
+{
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+}
+else
+{
+Console.WriteLine("max = ");
+Console.WriteLine(N3);
+}
